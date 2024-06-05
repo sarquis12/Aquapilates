301f4a7157baa3b1fedb5238d3a1d5a07b54a58a
eliasysb <eliass_sarquiss@hotmail.com>
2024-06-05 18:37:09 -0300

    permitir todos los hostings
    .

--- AquaPilates/settings.py
+++ AquaPilates/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--3-j9(y3que#28!y^k!m*q+59)1x2mupzy(yigd56-_$gruii=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
